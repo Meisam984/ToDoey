ea5f189ac4cf4581f9f0240a2229c43c156da52a
Meisam Rezaei <meysam.or.us@gmail.com>
2019-01-02 17:06:44 +0330

    Data persist through userDefaults

--- ToDoey/ToDoListViewController.swift
+++ ToDoey/ToDoListViewController.swift
@@ -8,33 +8,42 @@
 
 import UIKit
 
+// TODO: - create a UIViewTableController to add a new todo item and delete it -
+
 class ToDoListViewController: UITableViewController {
     
     var itemArray : [String] = []
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        // TODO: read itemArray from defaults
+        if let items = defaults.array(forKey: "ToDoListItemArray") as? [String] {
+            itemArray = items
+        }
     }
-    
-    ///////////////////////////////////////////////////
 
-    //MARK: Tableview Datasource Methods
+    // MARK: - Tableview Datasource Methods -
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //TODO: Number Of Rows
         return itemArray.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //TODO: Create the Reusable Cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
 
-    //MARK: Tableview Delegate Methods
+    // MARK: - Tableview Delegate Methods -
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
+        
+        // TODO: Define the accessory type of the selected item
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
@@ -43,18 +52,25 @@ class ToDoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    //MARK: Add New Items
+    //MARK: - Add New Items -
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        //TODO: create the UIAlertController to add a new item
         
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New ToDo", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            //what will happen once the user clicks the add item button
+            
+            //TODO: what will happen once the user clicks the add item button
+            
             self.itemArray.append(textField.text!)
+            //TODO: save itemArray in a UserDefaults var called defaults
+            self.defaults.set(self.itemArray, forKey: "ToDoListItemArray")
             self.tableView.reloadData()
         }
         
+        //TODO: UIAlertController TextField
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
